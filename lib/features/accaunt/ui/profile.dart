b267ab1194a23d7a8e67d1cf68b12b967e813587
emmanuelkbett3@gmail.com <piexie@kali>
2024-09-17 14:32:33 -0400

    updating the project

--- lib/features/accaunt/ui/profile.dart
+++ lib/features/accaunt/ui/profile.dart
@@ -8,16 +8,69 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            height: 300,
-            child: Stack(
-              children: [Image.asset(AppAssets.test1)],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              child: Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * .5,
+                  ),
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: SizedBox(
+                      height: 150,
+                      child: Image.asset(
+                        AppAssets.test1,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 110,
+                    left: 30,
+                    child: Container(
+                      margin: const EdgeInsets.all(5),
+                      child: const CircleAvatar(
+                        // backgroundColor: Colors.greenAccent,
+                        backgroundImage: AssetImage(AppAssets.schoolpic),
+                        radius: 40,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 200,
+                    left: 20,
+                    // right: 0,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Emmanuel Bett',
+                          style: Theme.of(context).primaryTextTheme.titleMedium,
+                        ),
+                        Text(
+                          '@emmanuel_dev2',
+                          style: Theme.of(context)
+                              .primaryTextTheme
+                              .bodyMedium
+                              ?.copyWith(color: Colors.grey),
+                        ),
+                        Text(
+                          'Playing long term games with long term people',
+                          style: Theme.of(context).primaryTextTheme.bodyMedium,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
