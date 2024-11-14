26cc299996cce49f73fcf7ca2e374bdea8ef6a13
piexie3 <emmanuelkbett3@gmail.com>
2024-11-14 23:17:56 +0300

    updated profile screen

--- lib/features/accaunt/ui/profile.dart
+++ lib/features/accaunt/ui/profile.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:meruinnovators/common/constants/assets_constants.dart';
 import 'package:meruinnovators/features/accaunt/ui/widgets/UserInfoCard.dart';
+import 'package:sizer/sizer.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-             UserInfoCard()
-            ],
-          ),
+        child: CustomScrollView(shrinkWrap: true,
+          slivers: [
+            const SliverToBoxAdapter(
+              child: UserInfoCard(),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'My Projects',
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto'
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
